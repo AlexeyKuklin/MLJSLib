18d005f8ddf7caab112b9a970d285e159318e376
AlexeyKuklin <AlexeyKuklin@gmail.com>
2013-12-27 21:55:10 +0300

    Update app_dt.js

--- app_dt.js
+++ app_dt.js
@@ -19,20 +19,20 @@ var dt = new tdt.DTree(ds);
 var ds2 = new tds.Dataset(['N', 'N', 'N', 'N']);
 ds2.add(new tds.Sample([5.1, 3.4, 1.5, 0.2], null));  //0
 var p = dt.predict(dt.root, ds2);
-console.log('should be 0 = ');
+console.log('It should be 0 = ');
 console.log(p);
 
 //
 ds2 = new tds.Dataset(['N', 'N', 'N', 'N']);
 ds2.add(new tds.Sample([7.1, 3.0, 5.9, 2.1], null));  //2
 p = dt.predict(dt.root, ds2);
-console.log('should be 2 = ');
+console.log('It should be 2 = ');
 console.log(p);
 
 ds2 = new tds.Dataset(['N', 'N', 'N', 'N']);
 ds2.add(new tds.Sample([6.3, 2.5, 4.9, 1.5], null));  //1
 p = dt.predict(dt.root, ds2);
-console.log('should be 1 = ');
+console.log('It should be 1 = ');
 console.log(p);
 
 
